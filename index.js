57db94a9861101423739247098a2f06e175fd3a9
shakilgit1 <bachelorshakil@gmail.com>
2023-11-05 22:09:51 +0600

    single data geting and loaded from database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -32,8 +32,13 @@ async function run() {
     // 
     app.get('/foods', async(req, res) =>{
        const cursor = foodCollections.find()
-       console.log(cursor);
        const result = await cursor.toArray();
+       res.send(result);
+    })
+    app.get('/foods/:id', async(req, res) =>{
+       const id = req.params.id;
+       const query = {_id: new ObjectId(id)};
+       const result = await foodCollections.findOne(query);
        res.send(result);
     })
   
