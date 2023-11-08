87e1827f93d91b79acf71aafe8222ebed679d47b
shakilgit1 <bachelorshakil@gmail.com>
2023-11-08 11:57:02 +0600

    logout users when token not match

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const client = new MongoClient(uri, {
 
 const verifyToken = async(req, res, next) =>{
   const token = req?.cookies?.token;
-  // console.log(req?.cookies?.token);
+  console.log(req?.cookies?.token);
   if(!token){
     return res.status(401).send({message: 'not authorized'})
   }
@@ -69,14 +69,18 @@ async function run() {
       res
       .cookie('token', token, {
         httpOnly: true,
-        secure: false
+        secure: process.env.NODE_ENV === "production" ? true: false,
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       })
       .send({success: true});
     })
 
     app.post('/logout', async(req, res) =>{
       // const user = req.body;
-      res.clearCookie('token', {maxAge: 0}).send({success:true});
+      res.clearCookie('token', {
+        maxAge: 0,
+        secure: process.env.NODE_ENV === "production" ? true: false,
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",}).send({success:true});
     })
 
     // get all foods
